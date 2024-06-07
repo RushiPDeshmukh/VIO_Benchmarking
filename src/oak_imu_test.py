9f4303841fa04d900ff596558c6aae9b74cfce3d
Jidnyesha <jpatil@wpi.edu>
2024-06-06 20:50:22 -0400

    save imu readings as csv

--- src/oak_imu_test.py
+++ src/oak_imu_test.py
@@ -2,7 +2,7 @@
 import cv2
 import depthai as dai
 import pandas as pd
-import math
+from tqdm import tqdm
 # from matplotlib.pyplot import 
 # Create pipeline
 pipeline = dai.Pipeline()
@@ -14,11 +14,16 @@ xlinkOut = pipeline.create(dai.node.XLinkOut)
 xlinkOut.setStreamName("imu")
 
 # enable ACCELEROMETER_RAW at 100 hz rate
-imu.enableIMUSensor(dai.IMUSensor.ACCELEROMETER_RAW, 100)
+imu.enableIMUSensor(dai.IMUSensor.ACCELEROMETER_RAW, 400)
 # enable GYROSCOPE_RAW at 100 hz rate
 imu.enableIMUSensor(dai.IMUSensor.GYROSCOPE_RAW, 100)
 # enable MAGNETOMETER_RAW at 100 hz rate
 imu.enableIMUSensor(dai.IMUSensor.MAGNETOMETER_RAW, 100)
+
+# Enable calibrated(check) sensors
+# imu.enableIMUSensor(dai.IMUSensor.ACCELEROMETER,400)
+# imu.enableIMUSensor(dai.IMUSensor.GYROSCOPE_CALIBRATED,100)
+# imu.enableIMUSensor(dai.IMUSensor.MAGNETOMETER_CALIBRATED,100)
 
 # it's recommended to set both setBatchReportThreshold and setMaxBatchReports to 20 when integrating in a pipeline with a lot of input/output connections
 # above this threshold packets will be sent in batch of X, if the host is not blocked and USB bandwidth is available
@@ -31,6 +36,8 @@ imu.setMaxBatchReports(10)
 # Link plugins IMU -> XLINK
 imu.out.link(xlinkOut.input)
 
+data={'Acc_ts':[],'Acc_x':[],'Acc_y':[],'Acc_z':[],'Gyro_ts':[],'Gyro_x':[],'Gyro_y':[],'Gyro_z':[],'Mag_ts':[],'Mag_x':[],'Mag_y':[],'Mag_z':[]}
+
 # Pipeline is defined, now we can connect to the device
 with dai.Device(pipeline) as device:
 
@@ -40,7 +47,7 @@ with dai.Device(pipeline) as device:
     # Output queue for imu bulk packets
     imuQueue = device.getOutputQueue(name="imu", maxSize=50, blocking=False)
     baseTs = None
-    while True:
+    for i in tqdm(range(1,1000,1)):
         imuData = imuQueue.get()  # blocking call, will wait until a new data has arrived
 
         imuPackets = imuData.packets
@@ -59,15 +66,28 @@ with dai.Device(pipeline) as device:
             magTs = timeDeltaToMilliS(magTs-baseTs)
             imuF = "{:.06f}"
             tsF  = "{:.03f}"
-            print(f"Base Ts = {(baseTs)} ms")
-            print(f"Accelerometer timestamp: {tsF.format(acceleroTs)} ms")
-            print(f"Accelerometer [m/s^2]: x: {imuF.format(acceleroValues.x)} y: {imuF.format(acceleroValues.y)} z: {imuF.format(acceleroValues.z)}")
-            print(f"Gyroscope timestamp: {tsF.format(gyroTs)} ms")
-            print(f"Gyroscope [rad/s]: x: {imuF.format(gyroValues.x)} y: {imuF.format(gyroValues.y)} z: {imuF.format(gyroValues.z)} ")
-            print(f"Magnetometer timestamp: {tsF.format(magTs)} ms")
-            print(f"Magnetometer [uT]: x: {imuF.format(magValues.x)} y: {imuF.format(magValues.y)} z: {imuF.format(magValues.z)} ")
-            print(f"Delta : Acc & Gyro = {tsF.format(acceleroTs -gyroTs)} ms , Gyro to Mag {tsF.format(gyroTs- magTs)} ms")
-            print("------------------------------------------------------------------------------------------------------------------------")
+            # print(f"Base Ts = {(baseTs)} ms")
+            # print(f"Accelerometer timestamp: {tsF.format(acceleroTs)} ms")
+            # print(f"Accelerometer [m/s^2]: x: {imuF.format(acceleroValues.x)} y: {imuF.format(acceleroValues.y)} z: {imuF.format(acceleroValues.z)}")
+            # print(f"Gyroscope timestamp: {tsF.format(gyroTs)} ms")
+            # print(f"Gyroscope [rad/s]: x: {imuF.format(gyroValues.x)} y: {imuF.format(gyroValues.y)} z: {imuF.format(gyroValues.z)} ")
+            # print(f"Magnetometer timestamp: {tsF.format(magTs)} ms")
+            # print(f"Magnetometer [uT]: x: {imuF.format(magValues.x)} y: {imuF.format(magValues.y)} z: {imuF.format(magValues.z)} ")
+            # print(f"Delta : Acc & Gyro = {tsF.format(acceleroTs -gyroTs)} ms , Gyro to Mag {tsF.format(gyroTs- magTs)} ms")
+            # print("------------------------------------------------------------------------------------------------------------------------")
+            data['Acc_ts'].append(acceleroTs)
+            data['Acc_x'].append(acceleroValues.x)
+            data['Acc_y'].append(acceleroValues.y)
+            data['Acc_z'].append(acceleroValues.z)
+            data['Gyro_ts'].append(gyroTs)
+            data['Gyro_x'].append(gyroValues.x)
+            data['Gyro_y'].append(gyroValues.y)
+            data['Gyro_z'].append(gyroValues.z)
+            data['Mag_ts'].append(magTs)
+            data['Mag_x'].append(magValues.x)
+            data['Mag_y'].append(magValues.y)
+            data['Mag_z'].append(magValues.z)
 
         if cv2.waitKey(1) == ord('q'):
             break
+    pd.DataFrame(data).to_csv('raw_imu.csv')
